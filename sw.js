cf3d4fb5c6ab7c8c6f30bdfdede781706a82cc3c
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 06:24:05 -0500

    Let one sale mix groceries, gas, and fishing after a single PIN.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v8";
+const CACHE = "sk-v9";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
