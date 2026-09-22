c12b94cb83215761b6becdb8d120e505bacd3837
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 14:51:53 -0500

    Keep the cart, tender, and Save button on screen in the right column.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v31";
+const CACHE = "sk-v32";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
