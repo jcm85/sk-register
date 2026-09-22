76de256b76ae7a34752f3ef42c08d01e618fc84d
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 08:56:23 -0500

    Add a search box to the catalog list.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v17";
+const CACHE = "sk-v18";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
