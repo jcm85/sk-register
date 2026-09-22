175f4fb07388825b52c427b4335efd9b17bc384c
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 08:20:51 -0500

    Show the catalog for the category picked in the dropdown.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v14";
+const CACHE = "sk-v15";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
