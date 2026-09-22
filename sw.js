07e5bb25193b133748e4e2f9b2ba99f83c2ea577
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 07:32:59 -0500

    Seed Groceries catalog from Pangasinan shelf photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v9";
+const CACHE = "sk-v10";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
