c6203ea2346889b35195a8519d7bd68b2789c083
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 14:55:01 -0500

    Hide zero-price gas from the All list until a pump price is set.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v32";
+const CACHE = "sk-v33";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
