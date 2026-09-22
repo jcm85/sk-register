55c4f9abd225e01aaa61dd869a28aaf69cff59f6
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 08:03:06 -0500

    Sell gas by the liter.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v13";
+const CACHE = "sk-v14";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
