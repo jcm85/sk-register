cbf07adb52fb7db19c3f97d2beeed984e2d892e7
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 07:56:12 -0500

    Replace sample grocery items with the shelf catalog.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v10";
+const CACHE = "sk-v11";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
