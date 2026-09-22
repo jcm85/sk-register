ae3aad87631deed05985cf3c631aaecf6ef9a36c
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 07:59:02 -0500

    Replace the gas catalog with unleaded and premium unleaded.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v12";
+const CACHE = "sk-v13";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
