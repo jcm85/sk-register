8b0ceb314ec53da6473d0b1ea2c5980932c006e1
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 06:14:41 -0500

    Lay out the register for an 11-inch iPad.

--- sw.js
+++ sw.js
@@ -1,6 +1,21 @@
+const CACHE = "sk-v8";
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("sk-v1").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
