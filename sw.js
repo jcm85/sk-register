9e99209e5f36490c662727637ed66d660c55018e
jcm85 <juncarlomarzo@gmail.com>
2026-09-22 08:32:17 -0500

    Add an English keyword to every catalog item.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v16";
+const CACHE = "sk-v17";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
