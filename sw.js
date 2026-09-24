9934f1866d8992c876b4b726a39cfc04a3b56957
jcm85 <juncarlomarzo@gmail.com>
2026-09-23 19:37:10 -0500

    Add Close day with per-store CSV downloads and house cash count.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v36";
+const CACHE = "sk-v39";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
