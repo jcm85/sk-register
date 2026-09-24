61bc1050682eac18a00c0ffb4343d5a98a0c3ded
jcm85 <juncarlomarzo@gmail.com>
2026-09-24 16:24:26 -0500

    Fix Close Download THIS store report for selected chip.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sk-v40";
+const CACHE = "sk-v41";
 self.addEventListener("install", e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
